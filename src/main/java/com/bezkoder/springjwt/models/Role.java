ffd2591522bd01260d37c657598b54ede261cff4
ajharulabedeen <ajharulabedeen@gmail.com>
2024-07-06 03:15:57 +0600

    config: jwt basic setup, alinged with web.

--- src/main/java/com/bezkoder/springjwt/models/Role.java
+++ src/main/java/com/bezkoder/springjwt/models/Role.java
@@ -3,14 +3,19 @@ package com.bezkoder.springjwt.models;
 import jakarta.persistence.*;
 import lombok.Data;
 
+import java.util.List;
+
 @Entity
 @Table(name = "roles")
 @Data
 public class Role {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private Integer id;
-  @Enumerated(EnumType.STRING)
-  @Column(length = 20)
-  private ERole name;
+  private Long id;
+
+  @Column(nullable=false, unique=true)
+  private String name;
+
+  @ManyToMany(mappedBy="roles")
+  private List<User> users;
 }
--- src/main/java/com/bezkoder/springjwt/models/User.java
+++ src/main/java/com/bezkoder/springjwt/models/User.java
@@ -1,12 +1,17 @@
 package com.bezkoder.springjwt.models;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "users"
@@ -15,6 +20,9 @@ import jakarta.validation.constraints.Size;
 //      @UniqueConstraint(columnNames = "email")
 //    }
     )
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -33,58 +41,20 @@ public class User {
   @Size(max = 120)
   private String password;
 
-  @ManyToMany(fetch = FetchType.LAZY)
-  @JoinTable(  name = "user_roles", 
-        joinColumns = @JoinColumn(name = "user_id"), 
-        inverseJoinColumns = @JoinColumn(name = "role_id"))
+  @Column(nullable=true)
+  private String name;
+
+  @ManyToMany(fetch = FetchType.EAGER, cascade=CascadeType.ALL)
+  @JoinTable(
+          name="users_roles",
+          joinColumns={@JoinColumn(name="USER_ID", referencedColumnName="ID")},
+          inverseJoinColumns={@JoinColumn(name="ROLE_ID", referencedColumnName="ID")})
   private Set<Role> roles = new HashSet<>();
 
-  public User() {
-  }
-
-  public User(String username, String email, String password) {
+  public User(String username, String name, String email, String password) {
+    this.name = name;
     this.username = username;
     this.email = email;
     this.password = password;
-  }
-
-  public Long getId() {
-    return id;
-  }
-
-  public void setId(Long id) {
-    this.id = id;
-  }
-
-  public String getUsername() {
-    return username;
-  }
-
-  public void setUsername(String username) {
-    this.username = username;
-  }
-
-  public String getEmail() {
-    return email;
-  }
-
-  public void setEmail(String email) {
-    this.email = email;
-  }
-
-  public String getPassword() {
-    return password;
-  }
-
-  public void setPassword(String password) {
-    this.password = password;
-  }
-
-  public Set<Role> getRoles() {
-    return roles;
-  }
-
-  public void setRoles(Set<Role> roles) {
-    this.roles = roles;
   }
 }
