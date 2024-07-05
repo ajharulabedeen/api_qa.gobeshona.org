409f8bed2a4793657ca826a3d8560a7e98ee8907
ajharulabedeen <ajharulabedeen@gmail.com>
2024-07-05 19:17:13 +0600

    test: lombok added

--- src/main/java/com/bezkoder/springjwt/models/Role.java
+++ src/main/java/com/bezkoder/springjwt/models/Role.java
@@ -1,9 +1,11 @@
 package com.bezkoder.springjwt.models;
 
 import jakarta.persistence.*;
+import lombok.Data;
 
 @Entity
 @Table(name = "roles")
+@Data
 public class Role {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
