ea08030fbade8576adfe4ab35407bd2a87c10a94
ajharulabedeen <ajharulabedeen@gmail.com>
2024-08-03 16:04:39 +0600

    feat: pom chnage

--- src/test/java/org/gobeshona/api/SpringBootSecurityJwtApplicationTests.java
+++ src/test/java/org/gobeshona/api/SpringBootSecurityJwtApplicationTests.java
@@ -1,13 +1,13 @@
-package org.gobeshona.api;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-public class SpringBootSecurityJwtApplicationTests {
-
-	@Test
-	public void contextLoads() {
-	}
-
-}
+//package org.gobeshona.api;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//public class SpringBootSecurityJwtApplicationTests {
+//
+//	@Test
+//	public void contextLoads() {
+//	}
+//
+//}
